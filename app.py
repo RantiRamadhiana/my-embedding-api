10a0dc8d30bf51ad9fa28b1ad30b47cc8d51072a
Ranti Ramadhiana <103477346+RantiRamadhiana@users.noreply.github.com>
2025-07-26 15:42:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer
+import os
 
 app = Flask(__name__)
 model = SentenceTransformer("distiluse-base-multilingual-cased-v1")
@@ -11,12 +12,12 @@ def index():
 @app.route("/embed", methods=["POST"])
 def embed():
     data = request.json
-    if not data or "text" not in data:
+    if not data or "inputs" not in data:
         return jsonify({"error": "Missing 'text' in request"}), 400
 
-    text = data["text"]
-    embedding = model.encode([text])[0].tolist()
-    return jsonify({"embedding": embedding})
+    text = data["inputs"]
+    embeddings = model.encode(text).tolist()
+    return jsonify({"embeddings": embeddings})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
